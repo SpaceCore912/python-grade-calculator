241a752340b79333d0109fdbd5b658cc4aad7ead
Tykhon Byshkin <tykhonbyshkin07@gmail.com>
2024-09-25 21:00:56 -0400

    fix minor bugs and imperfections

--- grade.py
+++ grade.py
@@ -1,18 +1,18 @@
 def calculator()->int:
 
     
-    enumber="Enter the number "
+    enumber="Enter the number of "
     egrade="Enter grade for "
     labs=int(input(enumber + "labs completed: "))
-    quizzes=int(input(egrade + "quizzes completed: "))
-    listassign=[int(input(egrade +"1: ")),int(input(egrade+"2: ")),int(input(egrade +"3: ")),int(input(egrade+"4: "))]
-    print(listassign)
+    quizzes=int(input(enumber + "quizzes completed: "))
+    lista=[int(input(egrade +"Assignment 1: ")),int(input(egrade+"Assignment 2: ")),int(input(egrade +"Assignment 3: ")),int(input(egrade+"Assignment 4: "))]
+    print(lista)
     
 
     midterm1=int(input(egrade+"Midterm 1: "))
     midterm2=int(input(egrade+"Midterm 2: "))
     final=int(input(egrade+"Final Exam: "))
-    midfinalprep=int(input(egrade+"MIdterms and Final Preparation"))
-    return (labs*(20/6)+quizzes*(15/6)+(list[1]+list[2]+list[3]+list[4])*4+midterm1*12.5+midterm2*12.5+final*18+midfinalprep*6)
-print(calculator())
+    midfinalprep=int(input(egrade+"Midterms and Final Preparation: "))
+    return round((labs*(20/6*100)+quizzes*(15/6*100)+(lista[0]+lista[1]+lista[2]+lista[3])*4+midterm1*12.5+midterm2*12.5+final*18+midfinalprep*6)/100)
+print("Your grade is: "+ str(calculator()))
     
